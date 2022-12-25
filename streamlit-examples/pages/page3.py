ce74277d7584b1764e1a5b36a93d7d6a6a48b5c6
Nidhal2022 <121226847+Nidhal2022@users.noreply.github.com>
2022-12-25 16:51:46 +0400

    Update page3.py

--- streamlit-examples/pages/page3.py
+++ streamlit-examples/pages/page3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 houses = pd.read_csv('/content/houses.csv')
 houses=houses.dropna()
