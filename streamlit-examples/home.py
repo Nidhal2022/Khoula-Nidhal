34128d56b3d274112c24dadba52e7cb9fecbe898
Nidhal2022 <121226847+Nidhal2022@users.noreply.github.com>
2022-12-25 16:53:53 +0400

    Update home.py

--- streamlit-examples/home.py
+++ streamlit-examples/home.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
-import matplotlib as plt
+
 import seaborn as sns
 
 houses = pd.read_csv('/content/houses.csv')
