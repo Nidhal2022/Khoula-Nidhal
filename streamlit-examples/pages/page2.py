d67f98838ee1aa8bbfba88ed78eb7713e6da1a38
Nidhal2022 <121226847+Nidhal2022@users.noreply.github.com>
2022-12-26 12:39:41 +0400

    Update page2.py

--- streamlit-examples/pages/page2.py
+++ streamlit-examples/pages/page2.py
@@ -28,7 +28,10 @@ def plot():
     st.plotly_chart(fig)
 
 
-    ax = sns.regplot(houses['fire insurance (R$)'],houses['rent amount (R$)']).get_figure()
+    #ax = sns.regplot(houses['fire insurance (R$)'],houses['rent amount (R$)']).get_figure()
+    ax=sns.regplot(x = "fire insurance (R$)",
+            y = "rent amount (R$)",
+            data = houses)
     st.pyplot(ax)
    
     
